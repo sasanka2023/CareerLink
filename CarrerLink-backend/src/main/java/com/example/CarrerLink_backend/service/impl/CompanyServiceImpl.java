ada443ed159fc5cdc7e820d1c289d34fefa653d3
sasanka2023 <gayathra12350@usci.ruh.ac.lk>
2024-12-27 17:00:17 +0530

    bug fixed using sonar

--- CarrerLink-backend/src/main/java/com/example/CarrerLink_backend/service/impl/CompanyServiceImpl.java
+++ CarrerLink-backend/src/main/java/com/example/CarrerLink_backend/service/impl/CompanyServiceImpl.java
@@ -27,6 +27,7 @@ public class CompanyServiceImpl implements CompanyService {
     private final ModelMapper modelMapper;
     private final TechnologyRepo technologyRepo;
     private final ClientRepo clientRepo;
+    private static final String ACTION_1 = " not found. ";
 
     @Override
     public List<CompanygetResponseDTO> getCompanies(String location, String category) {
@@ -58,44 +59,56 @@ public class CompanyServiceImpl implements CompanyService {
     public String updateCompany(CompanyUpdateRequestDTO companyUpdateRequestDTO) {
         if (companyRepository.existsById(companyUpdateRequestDTO.getId())) {
             Company company = modelMapper.map(companyUpdateRequestDTO, Company.class);
-            if (companyUpdateRequestDTO.getProducts() != null) {
-                for (Products product : company.getProducts()) {
-                    product.setCompany(company);
-                }
-            }
-            if (companyUpdateRequestDTO.getClients() != null) {
 
-                List<Client> clients = new ArrayList<>();
-                for (ClientDTO mappedClient : companyUpdateRequestDTO.getClients()) {
-                    Client client = clientRepo.findByClientName(mappedClient.getClientName())
-                            .orElseThrow(() -> new RuntimeException("Client with name " + mappedClient.getClientName() + " not found."));
-                    clients.add(client);
-                }
-                company.setClients(clients);
-            }
-            // Handle technologies
-            if (companyUpdateRequestDTO.getTechnologies() != null) {
-                List<Technology> technologies = new ArrayList<>();
-                for (TechnologyDTO mappedTechnology : companyUpdateRequestDTO.getTechnologies()) {
-                    Technology technology = technologyRepo.findByTechName(mappedTechnology.getTechName())
-                            .orElseThrow(() -> new RuntimeException("Technology with name " + mappedTechnology.getTechName() + " not found."));
-                    technologies.add(technology);
-                }
-                company.setTechnologies(technologies);
-            }
-            // Save the company
+            updateProducts(companyUpdateRequestDTO, company);
+            updateClients(companyUpdateRequestDTO, company);
+            updateTechnologies(companyUpdateRequestDTO, company);
             companyRepository.save(company);
             return "updated successfully";
         }
         else{
-            throw new RuntimeException("Company with ID " + companyUpdateRequestDTO.getId() + " not found.");
+            throw new RuntimeException("Company with ID " + companyUpdateRequestDTO.getId() + ACTION_1);
         }
     }
+
+    public void updateProducts(CompanyUpdateRequestDTO companyUpdateRequestDTO,Company company){
+        if (companyUpdateRequestDTO.getProducts() != null) {
+            for (Products product : company.getProducts()) {
+                product.setCompany(company);
+            }
+        }
+    }
+
+    public void updateClients(CompanyUpdateRequestDTO companyUpdateRequestDTO,Company company){
+        if (companyUpdateRequestDTO.getClients() != null) {
+
+            List<Client> clients = new ArrayList<>();
+            for (ClientDTO mappedClient : companyUpdateRequestDTO.getClients()) {
+                Client client = clientRepo.findByClientName(mappedClient.getClientName())
+                        .orElseThrow(() -> new RuntimeException("Client with name " + mappedClient.getClientName() + ACTION_1));
+                clients.add(client);
+            }
+            company.setClients(clients);
+        }
+    }
+
+    public void updateTechnologies(CompanyUpdateRequestDTO companyUpdateRequestDTO,Company company){
+        if (companyUpdateRequestDTO.getTechnologies() != null) {
+            List<Technology> technologies = new ArrayList<>();
+            for (TechnologyDTO mappedTechnology : companyUpdateRequestDTO.getTechnologies()) {
+                Technology technology = technologyRepo.findByTechName(mappedTechnology.getTechName())
+                        .orElseThrow(() -> new RuntimeException("Technology with name " + mappedTechnology.getTechName() + ACTION_1));
+                technologies.add(technology);
+            }
+            company.setTechnologies(technologies);
+        }
+    }
+
 
     @Override
     public void deleteCompany(Long id) {
         if (!companyRepository.existsById(id)) {
-            throw new RuntimeException("Company with ID " + id + " not found.");
+            throw new RuntimeException("Company with ID " + id + ACTION_1);
         }
         companyRepository.deleteById(id);
     }
