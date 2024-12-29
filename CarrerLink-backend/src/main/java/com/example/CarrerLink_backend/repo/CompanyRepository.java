9c94c86693438e63f4e7223c4c9e3f8dd6895352
Nipuni Ruwanpathirana <nipuni_ruwanpathirana@outlook.com>
2024-12-29 22:42:46 +0530

    CL-0007

--- CarrerLink-backend/src/main/java/com/example/CarrerLink_backend/repo/CompanyRepository.java
+++ CarrerLink-backend/src/main/java/com/example/CarrerLink_backend/repo/CompanyRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @EnableJpaRepositories
@@ -13,4 +14,7 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     List<Company> findByLocationAndCategory(String location, String category);
     List<Company> findByNameContainingIgnoreCase(String name);
+    List<Company> findByLocation(String location);
+    List<Company> findByCategory(String category);
+    Optional<Company> findByName(String name);
 }
