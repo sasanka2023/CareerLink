9da4651d3efde92d6ca158d72dedb38c2e36197d
L.P.Edirisinghe <154490097+lakindu2000@users.noreply.github.com>
2025-01-02 00:38:50 -0800

    Update ModelMapperConfig.java

--- CarrerLink-backend/src/main/java/com/example/CarrerLink_backend/config/ModelMapperConfig.java
+++ CarrerLink-backend/src/main/java/com/example/CarrerLink_backend/config/ModelMapperConfig.java
@@ -1,10 +1,7 @@
 package com.example.CarrerLink_backend.config;
 
 
-import com.example.CarrerLink_backend.dto.response.JobgetResponseDTO;
-import com.example.CarrerLink_backend.entity.Job;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,16 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper(){
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
-        // Add the type map for custom mappings
-        modelMapper.typeMap(Job.class, JobgetResponseDTO.class).addMappings(mapper -> {
-            mapper.map(src -> src.getCompany().getName(), JobgetResponseDTO::setCompany);
-        });
-
-        // Return the configured ModelMapper instance
-        return modelMapper;
-
+        return new ModelMapper();
     }
 }
