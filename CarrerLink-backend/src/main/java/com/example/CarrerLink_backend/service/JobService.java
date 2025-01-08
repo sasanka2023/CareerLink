696cd4750420d2f202bca70f0a979b7cceb821ce
lakindu <lakinduedirisinghe2000@gmail.com>
2025-01-07 21:34:27 -0800

    apply job fetures

--- CarrerLink-backend/src/main/java/com/example/CarrerLink_backend/service/JobService.java
+++ CarrerLink-backend/src/main/java/com/example/CarrerLink_backend/service/JobService.java
@@ -5,7 +5,7 @@ import com.example.CarrerLink_backend.dto.response.JobgetResponseDTO;
 import java.util.List;
 
 public interface JobService {
-    String saveJob(JobgetResponseDTO jobgetResponseDTO);
+    String saveJob(JobgetResponseDTO jobgetResponseDTO, Long companyId);
     List<JobgetResponseDTO> getJobs(String jobType, String company);
     List<JobgetResponseDTO> getJobs();
 
